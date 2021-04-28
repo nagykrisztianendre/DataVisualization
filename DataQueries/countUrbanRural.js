b69e0be1220c64104164f462ad86e99259bb060c
Nagy Krisztian Endre <krisztian.nagy@3ss.tv>
2021-04-28 21:57:25 +0300

    Piechart for hungarians too.

--- DataQueries/countUrbanRural.js
+++ DataQueries/countUrbanRural.js
@@ -1,5 +1,16 @@
 export function countUrbanRural(year) {
-    return fetch(`http://localhost:4200/countUrbanRural/${year}`, {
+    return fetch(`http://localhost:4200/countUrbanRural/all/${year}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .catch((error) => error.json());
+}
+
+export function countUrbanRuralMagyar(year) {
+    return fetch(`http://localhost:4200/countUrbanRural/magyar/${year}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
